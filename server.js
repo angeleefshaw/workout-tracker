8258a19c5967b12dec3e5aada76caeee9baa83b8
Angelee Shaw <angeleefshaw@gmail.com>
2021-03-01 09:58:19 -0500

    Mongo environment to Heroku

--- server.js
+++ server.js
@@ -16,7 +16,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect( "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect( "mongodb://localhost/workout", {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   useCreateIndex: true,
+   useFindAndModify: false
+  }
+);
 
 
 
